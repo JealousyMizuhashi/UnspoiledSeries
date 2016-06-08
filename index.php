298075f1a0c2463e744a02836f53a3c86a56df06
LEBRAZIDEC <francois.lebrazidec.idf@gmail.com>
2016-06-08 15:28:47 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -11,11 +11,23 @@
  
     <body ng-app="MyApp">
 	
-	<?php include_once("./php/connexion.php"); ?>
+	<?php //include_once("./php/connexion.php"); ?>
+	<?php
+	$host = 'mysql:host=localhost;dbname=unspoiledseries'; /* L'adresse du serveur */
+	$login = 'root'; /* Votre nom d'utilisateur */
+	$password = ''; /* Votre mot de passe */
+	try
+	{
+		$bdd = new PDO($host, $login, $password);
+	}
+	catch (Exception $e)
+	{
+		   die('Erreur : ' . $e->getMessage());
+	}
+?>
     <?php include_once("./php/entete.php"); ?>
-    
     <!-- Le corps -->
-    
+	
     <div id="corps">
         <h1 style="text-align:center">Unspoiled Series</h1>
       
@@ -27,57 +39,23 @@
 		
 		<nav id="menu">   			   
 			<div class="element_menu">
-	
 				<h3>Liste séries</h3>
-				<div ng-controller="MyController">
-				<ul>
-					<?php 
-						
-						$reponse = $bdd->query('SELECT * FROM serie');
-						
-						// On affiche chaque entrée une à une
-						while ($donnees = $reponse->fetch()) {
-						?>
-								<li><a ng-click="ShowHide()"> 
-								<?php 
-									echo $donnees['nom_serie'] 
-								?>
-								</a></li>
-							<div class="<?php echo $donnees['nom_serie'] ?>" ng-hide="IsHidden">
-							<label for="nombre-saison" id="label-nombre-saison">
-								Jusqu'à quel saison avez vous avancé dans <?php echo $donnees['nom_serie'] ?></br>
-							</label>
-							<input type="range" id="nombre-saison" name="nombre-saison" step="1" value="0" min="0" max="<?php echo $donnees['nb_saison_serie'] ?>" oninput="document.getElementById('AfficheRange').textContent=value" />
-							<span id="AfficheRange"></span> <!-- oninput = event JavaScript quand une valeur ou texte est changé, ici c'est le mouvement du curseur. On récupre la valeur dans l'input range, puis l'affiche avec span, équivalent d'un div sans saut de ligne -->								
-							</div> 
-						<?php
-						}
-					
-					?>
-				</ul>
-				<nav id="categories">  
-					<div class="<?php echo $donnees['nom_serie'] ?>" ng-hide="IsHidden">
-						<h3>Liste catégories</h3>
-						<ul>
-							<li><a href="#">Personnages</a></li>
-							<li><a href="#">Lieux</a></li>
-							<li><a href="#">Evènements</a></li>
-							<li><a href="#">Interviews</a></li>
-				
-					</div>		
-							
-							<?php
-								echo '<br/>';
-								$reponse->closeCursor();
-							?>
-							
-						</ul>
-					</div>    
-				</nav>	
-			</div>    
+				<form method="post" action="./php/index_timeline.php">
+				<?php 
+					$reponse = $bdd->query('SELECT * FROM serie');	
+					while ($donnees = $reponse->fetch())
+					{
+				?>
+					<input type="submit" name="serie_choisi" value="<?php echo $donnees['nom_serie'] ?>" id="<?php echo $donnees['nom_serie'] ?>" /><br />
+				<?php
+					}
+				?>
+				</form>
+					<?php
+						echo '<br/>';
+						$reponse->closeCursor();
+                    ?>  
 		</nav>
-		
-		
 
     </div>
     
